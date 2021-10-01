ec5a359d0f80c9a8ee4ec1036ea1c44849ebefed
Juan Cedeno <37850007+InfinityZ25@users.noreply.github.com>
2021-10-01 09:23:21 -0400

    Now it actually works

--- src/main/java/me/aleiv/core/paper/listeners/GlobalListener.java
+++ src/main/java/me/aleiv/core/paper/listeners/GlobalListener.java
@@ -30,25 +30,28 @@ import lombok.Getter;
 import me.aleiv.core.paper.Core;
 import me.aleiv.core.paper.events.GameTickEvent;
 import me.aleiv.core.paper.objects.Frame;
+import me.aleiv.core.paper.tablist.DedsafioTablistGenerator;
+import me.aleiv.core.paper.tablist.TablistLoadEvent;
+import me.aleiv.core.paper.tablist.TablistManager;
 import net.md_5.bungee.api.ChatColor;
 
-public class GlobalListener implements Listener{
-    
+public class GlobalListener implements Listener {
+
     Core instance;
 
     String black = Character.toString('\u3400');
     private @Getter static String star = Character.toString('\uEAA6');
     List<String> list = new ArrayList<>();
 
-    public GlobalListener(Core instance){
+    public GlobalListener(Core instance) {
         this.instance = instance;
     }
 
     @EventHandler
     public void onGameTick(GameTickEvent e) {
         Bukkit.getScheduler().runTask(instance, () -> {
-            if(e.getSecond() % 5 == 0){
-                Bukkit.getOnlinePlayers().forEach(player ->{
+            if (e.getSecond() % 5 == 0) {
+                Bukkit.getOnlinePlayers().forEach(player -> {
                     instance.sendHeader(player, getPlayerHeader(player));
                 });
             }
@@ -56,28 +59,29 @@ public class GlobalListener implements Listener{
     }
 
     @EventHandler
-    public void onQuit(PlayerQuitEvent e){
+    public void onQuit(PlayerQuitEvent e) {
         var game = instance.getGame();
         var recording = game.getRecording();
         var uuid = e.getPlayer().getUniqueId();
-        if(recording.containsKey(uuid)){
+        if (recording.containsKey(uuid)) {
             recording.remove(uuid);
         }
     }
 
     @EventHandler
-    public void onMove(PlayerMoveEvent e){
+    public void onMove(PlayerMoveEvent e) {
         var to = e.getTo();
-        if(to.getY() > 110) return;
+        if (to.getY() > 110)
+            return;
 
         var player = e.getPlayer();
-        if(to.getWorld().getName().toString().contains("lobby") && !list.contains(player.getName())){   
+        if (to.getWorld().getName().toString().contains("lobby") && !list.contains(player.getName())) {
             list.add(player.getName());
             var lobby = Bukkit.getWorld("lobby");
             var loc = new Location(lobby, 0.5, 126, 0.5, 90, -0);
 
             instance.showTitle(player, black, "", 10, 10, 10);
-            Bukkit.getScheduler().runTaskLater(instance, task ->{
+            Bukkit.getScheduler().runTaskLater(instance, task -> {
                 player.teleport(loc);
                 list.remove(player.getName());
             }, 15);
@@ -85,35 +89,45 @@ public class GlobalListener implements Listener{
     }
 
     @EventHandler
-    public void interact(PlayerInteractEvent e){
+    public void interact(PlayerInteractEvent e) {
         var action = e.getAction();
         var game = instance.getGame();
         var recording = game.getRecording();
         var uuid = e.getPlayer().getUniqueId();
-        if(recording.containsKey(uuid) && action == Action.RIGHT_CLICK_AIR){
+        if (recording.containsKey(uuid) && action == Action.RIGHT_CLICK_AIR) {
             var player = e.getPlayer();
             player.setVelocity(player.getLocation().getDirection());
         }
     }
 
-    //@EventHandler
-    public void playerMove(PlayerMoveEvent e){
+    // @EventHandler
+    public void playerMove(PlayerMoveEvent e) {
         var game = instance.getGame();
         var recording = game.getRecording();
         var uuid = e.getPlayer().getUniqueId();
-        if(recording.containsKey(uuid)){
+        if (recording.containsKey(uuid)) {
             var cinematic = recording.get(uuid);
             var frames = cinematic.getFrames();
             var loc = e.getPlayer().getLocation().clone();
-            var frame = new Frame(loc.getWorld().getName().toString(), loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
+            var frame = new Frame(loc.getWorld().getName().toString(), loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(),
+                    loc.getPitch());
             frames.add(frame);
         }
     }
 
-    public boolean shouldInteract(Player player){
+    @EventHandler
+    public void onTablistLoadEvent(TablistLoadEvent e) {
+        try {
+            e.setTablistManager(new TablistManager(new DedsafioTablistGenerator(instance)));
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    public boolean shouldInteract(Player player) {
         var lobby = Bukkit.getWorld("lobby");
 
-        if(player.getWorld() == lobby && !player.hasPermission("admin.perm")){
+        if (player.getWorld() == lobby && !player.hasPermission("admin.perm")) {
             return false;
         }
 
@@ -121,11 +135,12 @@ public class GlobalListener implements Listener{
     }
 
     @EventHandler
-    public void sit(PlayerInteractAtEntityEvent e){
+    public void sit(PlayerInteractAtEntityEvent e) {
         var entity = e.getRightClicked();
         var player = e.getPlayer();
-        
-        if(entity != null && entity instanceof ArmorStand stand && !stand.hasBasePlate() && stand.getPassengers().isEmpty()){
+
+        if (entity != null && entity instanceof ArmorStand stand && !stand.hasBasePlate()
+                && stand.getPassengers().isEmpty()) {
             stand.addPassenger(player);
         }
     }
@@ -139,7 +154,7 @@ public class GlobalListener implements Listener{
     }
 
     @EventHandler
-    public void onHunger(FoodLevelChangeEvent e){
+    public void onHunger(FoodLevelChangeEvent e) {
         var world = Bukkit.getWorld("lobby");
         var player = (Player) e.getEntity();
         if (player.getWorld() == world) {
@@ -188,9 +203,9 @@ public class GlobalListener implements Listener{
         }
 
     }
-    
+
     @EventHandler
-    public void onJoin(PlayerJoinEvent e){
+    public void onJoin(PlayerJoinEvent e) {
         var player = e.getPlayer();
         var lobby = Bukkit.getWorld("lobby");
         var loc = new Location(lobby, 0.5, 126, 0.5, 90, -0);
@@ -203,32 +218,31 @@ public class GlobalListener implements Listener{
     public void onEntityDamage(EntityDamageEvent e) {
         var entity = e.getEntity();
         if (entity instanceof Player player) {
-            if(!shouldInteract(player)){
+            if (!shouldInteract(player)) {
                 e.setCancelled(true);
             }
 
         }
     }
 
-    public String getPlayerHeader(Player player){
+    public String getPlayerHeader(Player player) {
         var manager = instance.getTeamManager();
         var header = new StringBuilder();
         var team = manager.getPlayerTeam(player.getUniqueId());
 
-        if(team != null){
+        if (team != null) {
             var points = team.getPoints() == null || team.getPoints() < 1 ? 0 : team.getPoints();
             header.append("\n");
             header.append(ChatColor.AQUA + "" + team.getTeamName() + " ");
             var uuids = team.getMembers();
-            for (UUID uuid : uuids){
+            for (UUID uuid : uuids) {
                 var p = Bukkit.getPlayer(uuid);
-                if(p != null){
+                if (p != null) {
                     header.append(ChatColor.YELLOW + p.getName() + " ");
                 }
             }
             header.append(ChatColor.WHITE + "" + points + ChatColor.RESET + star + " ");
         }
-        
 
         return header.toString();
     }
@@ -240,8 +254,5 @@ public class GlobalListener implements Listener{
             e.setCancelled(true);
         }
     }
-
-
-
 
 }
